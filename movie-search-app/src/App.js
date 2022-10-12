ddee303aaf06be0df31152c7a5768dadf6c434ca
Oyun-Erdene Munkhbaatar <oyunerdene.m@gmail.com>
2022-10-12 18:51:51 +0200

    added search function functionality

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -3,10 +3,24 @@ import './App.css';
 import SearchMovies from './SearchMovies';
 
 function App() {
+	const [movies, setMovies] = useState([]);
+
+	async function searchMovies(query) {
+		const url = `https://api.themoviedb.org/3/search/movie?api_key=66c8f4c6a035bc993b79a8a3de946da3&language=en-US&query=${query}&page=1&include_adult=false`;
+
+		try {
+			const res = await fetch(url);
+			const data = await res.json();
+			setMovies(data.results);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	return (
 		<div className="container">
 			<h1 className="title">React Movie Search</h1>
-			<SearchMovies />
+			<SearchMovies searchMovies={searchMovies} />
 			<ul></ul>
 		</div>
 	);
--- movie-search-app/src/SearchMovies.js
+++ movie-search-app/src/SearchMovies.js
@@ -1,10 +1,30 @@
-export default function SearchMovies() {
+import { useState } from 'react';
+
+export default function SearchMovies(props) {
+	const [queryString, setQueryString] = useState('');
+
+	function changeHandler(event) {
+		setQueryString(event.target.value);
+	}
+
+	function submitHandler(event) {
+		event.preventDefault();
+		props.searchMovies(queryString);
+		setQueryString('');
+	}
 	return (
-		<form className="form">
+		<form className="form" onSubmit={submitHandler}>
 			<label className="label" htmlFor="query">
 				Movie Name
 			</label>
-			<input className="input" type="text" name="query" placeholder="i.e. Jurassic Park" />
+			<input
+				className="input"
+				type="text"
+				name="query"
+				placeholder="i.e. Jurassic Park"
+				onChange={changeHandler}
+				value={queryString}
+			/>
 			<button className="button" type="submit">
 				Search
 			</button>
